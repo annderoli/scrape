87032241cf2e035252d34180986d82641cce1379
annderoli <annderoli@gmail.com>
2024-02-27 22:19:29 -0300

    tentando criar tabela

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import psycopg2
 
 
 # Buscando os dados e inserindo em um Dataframe
-
-url = ''
+url = 'https://www.activtrades.com/api/en/get-instruments'
 
 response = requests.get(url)
 
@@ -14,28 +13,38 @@ data = json.loads(response.text)
 
 df = pd.DataFrame(data['instruments'])
 
-# Conectando com o banco de dados
+df
 
-# Parâmetros de conexão
-dbname = 'scrap-db'
+# Conectando com o banco de dados
+dbname = 'scrap_db'
 user = 'postgres'
 password = '123'
-host = 'localhost'  # ou o endereço do seu servidor
-port = '5432'  # Porta padrão do PostgreSQL
+host = 'localhost' 
+port = '5432'  
 
+try:
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+    print("Conexão bem-sucedida!")
 
-conn = psycopg2.connect(dbname=dbname, 
-                        user=user,
-                        password=password, 
-                        host=host, 
-                        port=port)
+    # Criar um cursor
+    cur = conn.cursor()
 
+    # Executar uma consulta
+    table = cur.execute("CREATE TABLE ativos(code text, name int, description text);")
 
-cur.execute("""
-    CREATE TABLE Employee(
-        ID INT   PRIMARY KEY NOT NULL,
-        NAME TEXT NOT NULL,
-        EMAI TEXT NOT NULL)
-        """)
+    # Executar a consulta com executemany()
+    cur.executemany(table, df)
+    
+    # Recuperar os resultados
+    rows = cur.fetchall()
+    
+    # Exibir os resultados
+    for row in rows:
+        print(row)
 
-conn.commit()
+    # Fechar o cursor e a conexão
+    cur.close()
+    conn.close()
+
+except psycopg2.Error as e:
+    print("Erro ao conectar ao PostgreSQL:", e)
