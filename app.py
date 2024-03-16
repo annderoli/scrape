64850a6e0129276ea98adc0c25268f4f1e995a0f
annderoli <annderoli@gmail.com>
2024-03-16 18:33:59 -0300

    atualização

--- app.py
+++ app.py
@@ -15,54 +15,29 @@ df = pd.DataFrame(data['instruments'])
 
 df
 
-<<<<<<< HEAD
-# Parâmetros de conexão
-dbname = 'postgres'
-=======
+
 # Conectando com o banco de dados
 dbname = 'scrap_db'
->>>>>>> 87032241cf2e035252d34180986d82641cce1379
 user = 'postgres'
 password = '123'
 host = 'localhost' 
 port = '5432'  
 
-try:
-    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-    print("Conexão bem-sucedida!")
-
-<<<<<<< HEAD
 conn = psycopg2.connect(dbname=dbname, user=user,password=password, host=host, port=port)
 
+# Criar um cursor
 cur = conn.cursor()
 
+# Executar uma consulta
 cur.execute("SELECT * FROM ativos;")
 
+# Recuperar os resultados
 db = cur.fetchall()
 
+# Exibir os resultados
 for ativos in db:
     print(ativos)
-=======
-    # Criar um cursor
-    cur = conn.cursor()
 
-    # Executar uma consulta
-    table = cur.execute("CREATE TABLE ativos(code text, name int, description text);")
-
-    # Executar a consulta com executemany()
-    cur.executemany(table, df)
-    
-    # Recuperar os resultados
-    rows = cur.fetchall()
-    
-    # Exibir os resultados
-    for row in rows:
-        print(row)
->>>>>>> 87032241cf2e035252d34180986d82641cce1379
-
-    # Fechar o cursor e a conexão
-    cur.close()
-    conn.close()
-
-except psycopg2.Error as e:
-    print("Erro ao conectar ao PostgreSQL:", e)
+# Fechar o cursor e a conexão
+cur.close()
+conn.close()
