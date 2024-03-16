9618750db34290e9ab8aaeea02fd945ec0267256
annderoli <annderoli@gmail.com>
2024-03-16 18:07:49 -0300

    banco de dados conectado

--- app.py
+++ app.py
@@ -17,25 +17,22 @@ df = pd.DataFrame(data['instruments'])
 # Conectando com o banco de dados
 
 # Parâmetros de conexão
-dbname = 'scrap-db'
+dbname = 'postgres'
 user = 'postgres'
 password = '123'
 host = 'localhost'  # ou o endereço do seu servidor
 port = '5432'  # Porta padrão do PostgreSQL
 
 
-conn = psycopg2.connect(dbname=dbname, 
-                        user=user,
-                        password=password, 
-                        host=host, 
-                        port=port)
+conn = psycopg2.connect(dbname=dbname, user=user,password=password, host=host, port=port)
 
+cur = conn.cursor()
 
-cur.execute("""
-    CREATE TABLE Employee(
-        ID INT   PRIMARY KEY NOT NULL,
-        NAME TEXT NOT NULL,
-        EMAI TEXT NOT NULL)
-        """)
+cur.execute("SELECT * FROM ativos;")
+
+db = cur.fetchall()
+
+for ativos in db:
+    print(ativos)
 
 conn.commit()
