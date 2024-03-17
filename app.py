e6999956d5f951c4bc6a820857f23df414a7e2f7
annderoli <annderoli@gmail.com>
2024-03-17 10:55:22 -0300

    criação da tabela e insert dos ativos no banco de dados

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 
-
 # Buscando os dados e inserindo em um Dataframe
 url = 'https://www.activtrades.com/api/en/get-instruments'
 
@@ -14,9 +13,6 @@ data = json.loads(response.text)
 
 df = pd.DataFrame(data['instruments'])
 
-df
-
-
 # Conectando com o banco de dados
 dbname = 'annderoli'
 user = 'postgres'
@@ -25,35 +21,22 @@ host = 'localhost'
 port = '5432'  
 
 conn = psycopg2.connect(dbname=dbname, user=user,password=password, host=host, port=port)
-
-# Criar um cursor
 cur = conn.cursor()
 
-# Nome da tabela no banco de dados
-table_name = 'ativos'
-
 # Montar a query de criação da tabela
-create_table_query = f"""
-CREATE TABLE {table_name} (
-    code VARCHAR(10),
-    name VARCHAR(100),
-    description VARCHAR(100)
-);
-"""
-
-df.to_sql(table_name, conn, if_exists='replace', index=False)
+table_name = 'ativos'
+create_table_query = f"CREATE TABLE {table_name} (code VARCHAR(10),name VARCHAR(100),description VARCHAR(100));"
 
 # Executar a query para criar a tabela
 cur.execute(create_table_query)
 conn.commit()
 
-print(f"Tabela '{table_name}' criada com sucesso no banco de dados PostgreSQL.")
+# Insere os dados do DataFrame na tabela do PostgreSQL
+engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}')
+df.to_sql(table_name, engine, if_exists='replace', index=False)
 
 # Executar uma consulta
-table = cur.execute("CREATE TABLE ativos(code text, name int, description text);")
-
-# Executar a consulta com executemany()
-cur.executemany(table, df)
+table = cur.execute(f"SELECT * FROM {table_name}")
 
 # Recuperar os resultados
 rows = cur.fetchall()
