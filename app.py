0e8d3d7080350555f736df7b97dd3976162edae2
annderoli <annderoli@gmail.com>
2024-04-07 15:09:57 -0300

    Recebendo apenas ações EUA

--- app.py
+++ app.py
@@ -5,19 +5,35 @@ import psycopg2
 from sqlalchemy import create_engine
 
 # Buscando os dados e inserindo em um Dataframe
-url = 'https://www.activtrades.com/api/en/get-instruments'
 
-link = 'https://www.activtrades.com/api/en/get-instruments?instrumentType=0&isSpreadSwaps=1&instrument_group=10'
+url = 'https://www.activtrades.com/api/en/get-instruments?instrumentType=0&isSpreadSwaps=1&instrument_group=10'
 
 response = requests.get(url)
 
-data = json.loads(response.text)
+data = response.json()
 
-df = pd.DataFrame(data['instruments'])
+qtd_pages = data.get('last_page')
 
-filtro = df[df['name'].str.contains('.US')]
+qtd_pages
 
-filtro
+dfs = []
+
+# Pega todos os dados em todas as paginas
+for i in range(qtd_pages):
+    page = url + f'&page={i + 1}'
+    res = requests.get(page)
+    dat = res.json()
+    info  = pd.DataFrame(dat['data'])
+    # Remove as duas ultimas colunas do DataFrame
+    dfs.append(info.iloc[:, :-2])
+
+# Concatena todos os dados sepárados em um unico DataFrame
+df = pd.concat(dfs, ignore_index=True)
+
+# Remove limite de exibição dos dados
+pd.set_option('display.max_rows', None, 'display.max_columns', None, 'display.max_colwidth', None)
+
+df
 
 # Conectando com o banco de dados
 dbname = 'annderoli'
@@ -31,7 +47,7 @@ cur = conn.cursor()
 
 # Montar a query de criação da tabela
 table_name = 'ativos'
-create_table_query = f"CREATE TABLE {table_name} (code VARCHAR(10),name VARCHAR(100),description VARCHAR(100));"
+create_table_query = f"CREATE TABLE {table_name} (code VARCHAR(10),name VARCHAR(100));"
 
 # Executar a query para criar a tabela
 cur.execute(create_table_query)
